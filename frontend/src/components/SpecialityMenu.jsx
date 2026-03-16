3ebb02019149f5e9a62106ef56c2b281a1cc4702
TRK95_nguyetanh <86118897+TRK95@users.noreply.github.com>
2026-03-16 20:00:00 +0700

    your commit message

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const SpecialityMenu = () => {
   return (
-    <div>hello</div>
+    <div id='speciality'>
+        <h1>Find by Speciality</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptatem obcaecati hic placeat facilis, dolorem quo asperiores eius, officiis commodi magni, similique iusto dignissimos aperiam inventore tenetur corporis autem earum?</p>
+    </div>
   )
 }
 
